9bc352a86bf2dee3e0c8d60794f2df2de22d7faf
Havrysh Mykola <mykola.havrysh@ibykus.de>
2025-06-20 14:37:40 +0200

    1

---
+++ src/main/java/com/example/spring/spring_introduction/MyConfig.java
@@ -0,0 +1,11 @@
+package com.example.spring.spring_introduction;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("com.example.spring.spring_introduction")
+public class MyConfig {
+}
---
+++ src/main/java/com/example/spring/spring_introduction/Test6.java
@@ -0,0 +1,7 @@
+package com.example.spring.spring_introduction;
+
+public class Test6 {
+    public static void main(String[] args) {
+
+    }
+}
